4494e4acdafd74802230ffc469f6263e66a9cbb3
daniel.solis <daniel.solis@prodigious.cr>
2014-08-13 16:44:04 -0600

    Added spring mail sender code to send mail

--- src/main/java/com/fugitive/utils/mail/SendMailHelper.java
+++ src/main/java/com/fugitive/utils/mail/SendMailHelper.java
@@ -2,25 +2,29 @@ package com.fugitive.utils.mail;
 
 import java.util.Properties;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service( "mailer" )
 public class SendMailHelper implements ISendMailHelper {
 
     private Logger log = LoggerFactory.getLogger( getClass() );
-	
+
+    @Inject
+    @Named( value = "mailSender" )
+    private JavaMailSender javaMailSender;
+    
 	public void sendMail(String recipient, String subject, String body) 
 			throws SendMailException {
 
@@ -36,21 +40,23 @@ public class SendMailHelper implements ISendMailHelper {
 			log.info("Session properties : {}", sessionProperties);
 			log.info("Mail user : {}", System.getProperty("mail.user"));
 			
-			Message message = new MimeMessage(session);
+			SimpleMailMessage message = new SimpleMailMessage();
 			
-			InternetAddress[] address = {new InternetAddress(recipient)};
-		    message.setRecipients(Message.RecipientType.TO, address);
-		    message.setSubject(subject);
-		    message.setText(body);
+            message.setSubject( subject );
+            message.setText( body );
+
+            message.setTo( recipient );
+
+            log.debug( "mail content {}", body );
 		    
-		    Transport.send(message);
+		    javaMailSender.send( message );
 		    
 			log.info("Mail message sent to: {}", recipient);
 		    
 		} catch (NamingException e) {
 			log.error("Error getting JNDI mail session", e);
 			throw new SendMailException("Couldn't get mail session: " + e.getMessage());
-		} catch (MessagingException e) {
+		} catch (MailException e) {
 			log.error("Error sending mail message", e);
 			throw new SendMailException("Couldn't send mail message: " + e.getMessage());
 		}
