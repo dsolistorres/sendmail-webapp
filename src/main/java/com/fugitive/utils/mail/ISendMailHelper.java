4be62868a0107534dc25498019f4959d1d7e50e2
dsolis <dsolistorres@gmail.com>
2014-08-19 22:37:00 -0600

    Changed message type to mime

--- src/main/java/com/fugitive/utils/mail/ISendMailHelper.java
+++ src/main/java/com/fugitive/utils/mail/ISendMailHelper.java
@@ -2,6 +2,14 @@ package com.fugitive.utils.mail;
 
 public interface ISendMailHelper {
 
+    String EMAIL_FROM_ADDRESS = "email.from.address";
+
+    String EMAIL_FROM_NAME = "email.from.name";
+
+    String EMAIL_USER_NAME = "email.user.name";
+
+    String EMAIL_USER_PASSWORD = "email.user.password";
+    
 	void sendMail(String recipient, String subject, String body) 
 		throws SendMailException;
 	
--- src/main/java/com/fugitive/utils/mail/SendMailHelper.java
+++ src/main/java/com/fugitive/utils/mail/SendMailHelper.java
@@ -1,18 +1,21 @@
 package com.fugitive.utils.mail;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
@@ -38,14 +41,21 @@ public class SendMailHelper implements ISendMailHelper {
 			
 			Properties sessionProperties = session.getProperties();
 			log.info("Session properties : {}", sessionProperties);
-			log.info("Mail user : {}", System.getProperty("mail.user"));
-			
-			SimpleMailMessage message = new SimpleMailMessage();
+			log.info("Mail user : {}", System.getProperty(EMAIL_USER_NAME));
+		
+			MimeMessage message = javaMailSender.createMimeMessage();;
 			
             message.setSubject( subject );
             message.setText( body );
 
-            message.setTo( recipient );
+	        String fromAddress = System.getProperty( EMAIL_FROM_ADDRESS );
+	        String fromName =  System.getProperty( EMAIL_FROM_NAME );
+            InternetAddress from = new InternetAddress( fromAddress, fromName );
+
+            message.setFrom( from );
+
+            InternetAddress to = new InternetAddress( recipient.trim() );            
+            message.setRecipient(Message.RecipientType.TO, to);
 
             log.debug( "mail content {}", body );
 		    
@@ -56,7 +66,7 @@ public class SendMailHelper implements ISendMailHelper {
 		} catch (NamingException e) {
 			log.error("Error getting JNDI mail session", e);
 			throw new SendMailException("Couldn't get mail session: " + e.getMessage());
-		} catch (MailException e) {
+		} catch (MessagingException | UnsupportedEncodingException e) {
 			log.error("Error sending mail message", e);
 			throw new SendMailException("Couldn't send mail message: " + e.getMessage());
 		}
